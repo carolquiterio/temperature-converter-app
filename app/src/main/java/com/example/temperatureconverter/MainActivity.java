2d5d221fd02238127a19278995103453cc2201a6
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-06-25 16:21:31 -0300

    Changing activity with button

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.temperatureconverter;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMensagem;
+    EditText edtCelsius;
+    Button btnConverter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMensagem = (TextView)findViewById(R.id.tvMensagem);
+        edtCelsius = (EditText)findViewById(R.id.edtCelsius);
+        btnConverter = (Button)findViewById(R.id.btnConverter);
+
+        btnConverter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edtCelsius.getText().toString().equals(""))
+                {
+                    tvMensagem.setText("Escreva um número para continuar!");
+                }
+                else {
+                    tvMensagem.setText("");
+
+                    double varCelsius = Double.parseDouble((edtCelsius.getText().toString()));
+
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    Bundle params = new Bundle();
+
+                    params.putDouble("celsius", varCelsius);
+
+                    intent.putExtras(params);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
